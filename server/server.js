19c80064833e7734e02a0fb63334cf0004c6aa56
Abhishek <abhishekmamta2002@gmail.com>
2025-06-23 22:21:01 +0530

    your message

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js'
+import imageRouter from './routes/imageRoutes.js'
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.use(cors())
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(` ${req.method} request to ${req.originalUrl}`)
+  next()
+})
+
+app.use('/api/user', userRouter)
+app.use('/api/image', imageRouter)
+
+app.get('/', (req, res) => res.send("API Working"))
+
+app.use((req, res) => {
+  res.status(404).send(`Cannot ${req.method} ${req.url}`)
+})
+
+const startServer = async () => {
+  await connectDB()
+  app.listen(PORT, () => console.log(` Server running on port ${PORT}`))
+}
+
+startServer()
